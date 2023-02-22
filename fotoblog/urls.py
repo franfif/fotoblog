75a37a95d401580e7cf25263b9444ed09f587455
franfif <francis.barrow@gmail.com>
2023-02-22 15:37:47 -0500

    refactor login, logout, passwordchange and passwordchangedone to generic views

--- fotoblog/urls.py
+++ fotoblog/urls.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.contrib.auth.views import LoginView, \
+    LogoutView, PasswordChangeView, PasswordChangeDoneView
 from django.urls import path
 import authentication.views
 import blog.views
@@ -6,7 +8,20 @@ import blog.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', authentication.views.login_page, name='login'),
-    path('logout/', authentication.views.logout_user, name='logout'),
-    path('home/', blog.views.home_page, name='home'),
+    # path('', authentication.views.LoginPageView.as_view(), name='login'),
+    path('', LoginView.as_view(
+        template_name='authentication/login.html',
+        redirect_authenticated_user=True,
+    ), name='login'),
+    # path('logout/', authentication.views.logout_user, name='logout'),
+    path('logout/', LogoutView.as_view(
+        template_name='authentication/logout.html',
+    ), name='logout'),
+    path('home/', blog.views.HomePageView.as_view(), name='home'),
+    path('password-change/', PasswordChangeView.as_view(
+        template_name='authentication/password_change.html',
+    ), name='password_change'),
+    path('password-change/done/', PasswordChangeDoneView.as_view(
+        template_name='authentication/password_change_done.html',
+    ), name='password_change_done'),
 ]
