034a3e0e89e2846aaf9b0307393156bef80c11bb
franfif <francis.barrow@gmail.com>
2023-02-22 15:35:36 -0500

    refactor home view to class-based view

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,17 @@
-from django.contrib.auth.decorators import login_required
+# from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from django.views.generic import View
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
-@login_required
-def home_page(request):
-    return render(request, 'blog/home.html')
+# @login_required
+# def home_page(request):
+#     return render(request, 'blog/home.html')
+
+class HomePageView(LoginRequiredMixin, View):
+    template_name = 'blog/home.html'
+    login_url = ''
+    # redirect_field_name = 'redirect_to'
+
+    def get(self, request):
+        return render(request, self.template_name)
