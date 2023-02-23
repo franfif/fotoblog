9b744d8146b4c16e6c3d91ced830f81335770a15
franfif <francis.barrow@gmail.com>
2023-02-23 16:30:34 -0500

    update blog views

--- blog/views.py
+++ blog/views.py
@@ -1,17 +1,34 @@
-# from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
-from django.views.generic import View
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+# from django.views.generic import View
+# from django.contrib.auth.mixins import LoginRequiredMixin
+from . import forms, models
 
 
-# @login_required
-# def home_page(request):
-#     return render(request, 'blog/home.html')
+@login_required
+def home_page(request, user_id=None):
+    if user_id:
+        photos = models.Photo.objects.filter(uploader=user_id)
+    else:
+        photos = models.Photo.objects.all()
+    return render(request, 'blog/home.html', context={'photos': photos})
 
-class HomePageView(LoginRequiredMixin, View):
-    template_name = 'blog/home.html'
-    login_url = ''
-    # redirect_field_name = 'redirect_to'
+# class HomePageView(LoginRequiredMixin, View):
+#     template_name = 'blog/home.html'
+#     login_url = ''
+#     # redirect_field_name = 'redirect_to'
 
-    def get(self, request):
-        return render(request, self.template_name)
+    # def get(self, request):
+    #     return render(request, self.template_name)
+
+
+def photo_upload(request):
+    form = forms.PhotoForm()
+    if request.method == 'POST':
+        form = forms.PhotoForm(request.POST, request.FILES)
+        if form.is_valid():
+            photo = form.save(commit=False)
+            photo.uploader = request.user
+            photo.save()
+            return redirect('home')
+    return render(request, 'blog/photo_upload.html', context={'form': form})
