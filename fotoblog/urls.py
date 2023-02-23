6a89c2675eb59f351450c490976cc2ec31f5dafd
franfif <francis.barrow@gmail.com>
2023-02-23 16:30:57 -0500

    update url patterns to add home view and files

--- fotoblog/urls.py
+++ fotoblog/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.auth.views import LoginView, \
     LogoutView, PasswordChangeView, PasswordChangeDoneView
@@ -19,7 +21,8 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(
         template_name='authentication/logout.html',
     ), name='logout'),
-    path('home/', blog.views.HomePageView.as_view(), name='home'),
+    path('home/', blog.views.home_page, name='home'),
+    path('home/<int:user_id>/', blog.views.home_page, name='home'),
     path('password-change/', PasswordChangeView.as_view(
         template_name='authentication/password_change.html',
     ), name='password_change'),
@@ -27,4 +30,8 @@ urlpatterns = [
         template_name='authentication/password_change_done.html',
     ), name='password_change_done'),
     path('signup/', authentication.views.signup_page, name='signup'),
+    path('photo_upload/', blog.views.photo_upload, name='photo_upload'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
