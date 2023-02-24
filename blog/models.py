359ac9efd636f165b26069edfd568533b335219a
franfif <francis.barrow@gmail.com>
2023-02-24 14:32:48 -0500

    add author to Blog schema

--- blog/models.py
+++ blog/models.py
@@ -13,5 +13,6 @@ class Blog(models.Model):
     photo = models.ForeignKey(Photo, on_delete=models.SET_NULL, null=True, blank=True)
     title = models.CharField(max_length=128)
     content = models.CharField(max_length=5000)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     date_created = models.DateTimeField(auto_now_add=True)
     starred = models.BooleanField(default=False)
