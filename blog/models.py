b3bb86e36d61dd4d954515533dc85e62392e8a28
franfif <francis.barrow@gmail.com>
2023-02-28 16:20:04 -0500

    add resize_image functionality

--- blog/models.py
+++ blog/models.py
@@ -1,12 +1,25 @@
+from PIL import Image
+
 from django.conf import settings
 from django.db import models
 
 
 class Photo(models.Model):
+    IMAGE_MAX_SIZE = (400, 400)
+
     image = models.ImageField()
     caption = models.CharField(max_length=128, blank=True)
     uploader = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     date_created = models.DateTimeField(auto_now_add=True)
+
+    def resize_image(self):
+        image = Image.open(self.image)
+        image.thumbnail(self.IMAGE_MAX_SIZE)
+        image.save(self.image.path)
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        self.resize_image()
 
 
 class Blog(models.Model):
